f43055a2b195cf38ef341d498bc2460d23ea6199
Kevin Enloe <enloe92@gmail.com>
2020-11-16 14:37:48 -0700

    first draft

---
+++ main.js
@@ -0,0 +1,42 @@
+/* eslint-disable quotes */
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
+'use strict';
+
+
+
+$(function addItem(){
+  $('#js-shopping-list-form').on('submit',function(event){
+    event.preventDefault();    
+    
+    const newListItem = $(event.currentTarget).find(`input[id='shopping-list-entry']`).val();
+    console.log(newListItem);
+
+    $('.shopping-list').append(`<li>
+    <span class="shopping-item">${newListItem}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle">
+        <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+      </button>
+    </div>
+  </li>`)
+  });  
+});
+
+$(function checkMark(){
+  $('.shopping-list').on('click','.shopping-item-toggle',function(e){
+    e.preventDefault(); 
+    $(e.currentTarget).closest('li').find(`.shopping-item`).toggleClass('shopping-item__checked');
+    
+  });
+});
+
+$(function deleteItem(){
+  $(`.shopping-list`).on(`click`,`.shopping-item-delete`,function(e){
+    e.preventDefault(); 
+    $(e.currentTarget).closest('li').remove();
+  });
+});
